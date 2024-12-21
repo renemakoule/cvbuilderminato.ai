f65f7557bf685b63dda8f8c42c3c298c79cb5ea0
renemakoule <renemakoule83@gmail.com>
2024-12-21 15:36:49 +0100

    AdSystem

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,42 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'yt3.ggpht.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'pbs.twimg.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'tzupkqiuzvqpqvyevywz.supabase.co',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com', // Add Unsplash domain here
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    optimizeFonts: false,  // Désactiver l'optimisation des polices Google
+},
 };
-
 export default nextConfig;
