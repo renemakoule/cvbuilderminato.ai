211b372082ddbfaae65ec3fd3b751489b374ad9e
renemakoule <renemakoule83@gmail.com>
2024-12-22 11:04:33 +0100

    CV

--- app/page.tsx
+++ app/page.tsx
@@ -106,13 +106,13 @@ export default function FUIHeroSectionWithLogoClouds() {
               </li>
             ))}
           </div>
-          <Link href='/Cv' target="_blank">
+          {/* <Link href='/Cv' target="_blank">
           <li className="order-2 py-5 md:py-0">
             <button className="inline-flex h-12 items-center justify-center font-geist rounded-md border border-gray-800 bg-gradient-to-t from-[#8678f9] from-0% to-[#c7d2fe] px-6 font-medium text-gray-950 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 focus:ring-offset-gray-50 ">
               Create Cv
             </button>
           </li>
-          </Link>
+          </Link> */}
         </ul>
       </nav>
       </div>
