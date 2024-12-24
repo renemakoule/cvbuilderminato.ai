67baa0babe7b7dc51c1d754854c36cc5522ace88
renemakoule <renemakoule83@gmail.com>
2024-12-24 18:28:48 +0100

    Authenticate

--- app/page.tsx
+++ app/page.tsx
@@ -112,12 +112,10 @@ export default function FUIHeroSectionWithLogoClouds() {
       <section className="py-28 z-10">
         <div className="max-w-screen-xl mx-auto text-gray-600 gap-x-12 items-center justify-between overflow-hidden md:flex md:px-8">
           <div className="flex-none space-y-5 px-4 sm:max-w-lg md:px-0 lg:max-w-xl">
-            <Link href='/Cv' target="_blank">
             <h1 className="text-sm  text-gray-400 hover:text-purple-400 group font-geist mr-auto px-5 py-2 bg-gradient-to-tr from-zinc-300/5 via-gray-400/5 to-transparent  border-[2px] border-white/5 hover:border-purple-500 rounded-3xl w-fit">
               Create your Cv at $0
               <ChevronRight className="inline w-4 h-4 ml-2 group-hover:translate-x-1 duration-300" />
             </h1>
-            </Link>
             <h2 className="text-3xl md:text-4xl font-geist font-normal tracking-tighter lg:text-4xl text-transparent bg-clip-text bg-[linear-gradient(180deg,_#FFF_0%,_rgba(255,_255,_255,_0.00)_202.08%)]">
             We help content creators, brands and persona grow and earn money with our AI-powered Minato.ai platform
             </h2>
